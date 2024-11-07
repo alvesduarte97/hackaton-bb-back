f2567ec4b30f33b761a65632afae11146c240453
alvesduarte97 <alvesduarte97@gmail.com>
2024-11-07 19:33:47 -0300

    add email

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
+
+
 import express from 'express';
 const app = express();
-const port = 3000;
 
-import { applicationDefault, cert, initializeApp, ServiceAccount } from 'firebase-admin/app';
-import { getFirestore, Timestamp, FieldValue, Filter } from 'firebase-admin/firestore';
+
+import {  cert, initializeApp, ServiceAccount } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
 import serviceAccount from './resources/firebase-key.json' with { type: "json" };
 
-initializeApp({
+if(process.env.GOOGLE_APPLICATION_CREDENTIALS && process.env.GOOGLE_APPLICATION_CREDENTIALS != ''){
+  initializeApp();
+}else {
+  initializeApp({
     credential: cert(serviceAccount as ServiceAccount)
-});
+  });
+}
 
 const db = getFirestore();
 
@@ -18,13 +24,27 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/upsert-location-status', async (req, res) => {
-  const status = req.query.status as string;
-  const locationId = req.query.location as string;
+app.get('/upsert-location-status/:imei/:ultrassonic', async (req, res) => {
+    console.log("EXECUTANDO UPSERT")
+  const ultrassonic = req.params.ultrassonic as string;
+  const imei = req.params.imei as string;
   // http://localhost:3000/upsert-location-status?status=full&location=123132
-  await docRefBoxLocation.doc(locationId).set({
+    const status =  Number(ultrassonic) <= 10 ? 'full' : 'empty'
+
+  await docRefBoxLocation.doc(imei).set({
     status: status,
   });
+
+  if (status == 'full'){
+    console.log("ENVIANDO EMAIL")
+    await sendEmail({
+        from: 'dm.inovaccoes@gmail.com',
+        to: 'alvesduarte97@gmail.com',
+        subject: 'Ponto de Coleta FULL',
+        text: 'Ponto de coleta cheio',
+        html: '<p>O ponto de coleta esta <b>cheio</b></p>',
+    });
+  }
   res.send('Status updated successfully');
 });
 
@@ -34,6 +54,49 @@ app.get('/get-location', async (req, res) => {
   res.send(doc.data());
 });
 
+const port = 3000;
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
+
+// export const server = onRequest(app) 
+
+
+import nodemailer from 'nodemailer';
+
+// Interface para definir os parâmetros de envio de email
+interface EmailOptions {
+  from: string;
+  to: string;
+  subject: string;
+  text: string;
+  html?: string;
+}
+
+// Função para enviar o email
+async function sendEmail(emailOptions: EmailOptions) {
+  console.log('pass', process.env.EMAIL_PASS)
+  // Configuração do transportador de email
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // Ou outro provedor de email SMTP
+    auth: {
+      user: 'dm.inovaccoes@gmail.com', // Email do remetente
+      pass: 'yblk bgzh jvim tvyl', // Senha do remetente
+    },
+  });
+
+  // Envia o email com as opções definidas
+  try {
+    const info = await transporter.sendMail({
+      from: emailOptions.from,
+      to: emailOptions.to,
+      subject: emailOptions.subject,
+      text: emailOptions.text,
+      html: emailOptions.html,
+    });
+
+    console.log('Email enviado com sucesso:', info.response);
+  } catch (error) {
+    console.error('Erro ao enviar o email:', error);
+  }
+}
