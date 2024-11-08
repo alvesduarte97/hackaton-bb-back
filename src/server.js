c0dd16a14ad9eae53bc8d2951a59050ae72364a1
alvesduarte97 <alvesduarte97@gmail.com>
2024-11-07 23:39:25 -0300

    initialize app

--- src/server.js
+++ src/server.js
@@ -8,14 +8,14 @@ const port = parseInt(process.env.PORT) || 3000;
 app.use(cors())
 app.use(express.json())
 
-if(process.env.GOOGLE_APPLICATION_CREDENTIALS && process.env.GOOGLE_APPLICATION_CREDENTIALS != ''){
-  initializeApp();
-}else {
-  const serviceAccount = require('./resources/firebase-key.json');
-  initializeApp({
-    credential: cert(serviceAccount)
-  });
-}
+// if(process.env.GOOGLE_APPLICATION_CREDENTIALS && process.env.GOOGLE_APPLICATION_CREDENTIALS != ''){
+initializeApp();
+// }else {
+//   const serviceAccount = require('./resources/firebase-key.json');
+//   initializeApp({
+//     credential: cert(serviceAccount)
+//   });
+// }
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
