c4c410fe715c616768e0d39c2987e887d0007890
alvesduarte97 <alvesduarte97@gmail.com>
2024-11-07 22:48:47 -0300

    deploy

---
+++ src/server.js
@@ -0,0 +1,103 @@
+
+
+import express from 'express';
+const app = express();
+
+
+import {  cert, initializeApp } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import serviceAccount from './resources/firebase-key.json' with { type: "json" };
+
+const port = 3000;
+
+if(process.env.GOOGLE_APPLICATION_CREDENTIALS && process.env.GOOGLE_APPLICATION_CREDENTIALS != ''){
+  initializeApp();
+}else {
+  initializeApp({
+    credential: cert(serviceAccount)
+  });
+}
+
+const db = getFirestore();
+
+const docRefBoxLocation = db.collection('locations')
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/upsert-location-status/:imei/:ultrassonic', async (req, res) => {
+    console.log("EXECUTANDO UPSERT")
+  const ultrassonic = req.params.ultrassonic;
+  const imei = req.params.imei;
+  // http://localhost:3000/upsert-location-status?status=full&location=123132
+    const status =  Number(ultrassonic) <= 10 ? 'full' : 'empty'
+
+  await docRefBoxLocation.doc(imei).set({
+    status: status,
+  });
+
+  if (status == 'full'){
+    console.log("ENVIANDO EMAIL")
+    await sendEmail({
+        from: 'dm.inovaccoes@gmail.com',
+        to: 'alvesduarte97@gmail.com',
+        subject: 'Ponto de Coleta FULL',
+        text: 'Ponto de coleta cheio',
+        html: '<p>O ponto de coleta esta <b>cheio</b></p>',
+    });
+  }
+  res.send('Status updated successfully');
+});
+
+app.get('/get-location', async (req, res) => {
+  const locationId = req.query.location;
+  const doc = await docRefBoxLocation.doc(locationId).get();
+  res.send(doc.data());
+});
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+// export const server = onRequest(app) 
+
+
+import nodemailer from 'nodemailer';
+
+// Interface para definir os parâmetros de envio de email
+// interface EmailOptions {
+//   from: string;
+//   to: string;
+//   subject: string;
+//   text: string;
+//   html?: string;
+// }
+
+// Função para enviar o email
+async function sendEmail(emailOptions) {
+  console.log('pass', process.env.EMAIL_PASS)
+  // Configuração do transportador de email
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // Ou outro provedor de email SMTP
+    auth: {
+      user: 'dm.inovaccoes@gmail.com', // Email do remetente
+      pass: 'yblk bgzh jvim tvyl', // Senha do remetente
+    },
+  });
+
+  // Envia o email com as opções definidas
+  try {
+    const info = await transporter.sendMail({
+      from: emailOptions.from,
+      to: emailOptions.to,
+      subject: emailOptions.subject,
+      text: emailOptions.text,
+      html: emailOptions.html,
+    });
+
+    console.log('Email enviado com sucesso:', info.response);
+  } catch (error) {
+    console.error('Erro ao enviar o email:', error);
+  }
+}
